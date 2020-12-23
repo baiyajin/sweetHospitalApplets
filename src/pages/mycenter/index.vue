5d3c144e808ab268b3400a80bc7aa1fbbba2e0bc
baiyajin <baiyajin110@qq.com>
2020-12-24 02:34:41 +0800

    更新

--- src/pages/mycenter/index.vue
+++ src/pages/mycenter/index.vue
@@ -1,208 +1,212 @@
 <template>
-	<view class="content">
-		<view>
-			<image class="padding_top_bottom_10" mode="widthFix" src="~@/static/CC_10.png"></image>
-		</view>
-		<view>
-			<swiper
-					:style="{width: width + 'px'}"
-					class="swiper"
-					:indicator-dots="indicatorDots"
-					indicator-color="white"
-					indicator-active-color="#00adaf"
-					:autoplay="autoplay"
-					:interval="interval"
-					:duration="duration">
-				<swiper-item>
-					<image class="ele_center" mode="aspectFit" src="~@/static/CC_13.jpg"></image>
-				</swiper-item>
-				<swiper-item>
-					<image class="ele_center" mode="aspectFit" src="~@/static/CC_13.jpg"></image>
-				</swiper-item>
-				<swiper-item>
-					<image class="ele_center" mode="aspectFit" src="~@/static/CC_13.jpg"></image>
-				</swiper-item>
-			</swiper>
-		</view>
-		<view class="uni-flex padding_top_bottom_10">
-			<view class="uni-flex-item" style="width: 150px">
-				<image class="image_view ele_center" style="width: 80px;height:80px;" src="~@/static/CC_23.png"></image>
-			</view>
-			<view class="uni-flex-item">
-				<view v-for="(i, d) in message" :key="d" class="list_view">
-					<view class="status_view" v-if="!(d%2)"></view>
-					<view class="title">{{i.title}}</view>
-					<view class="content">{{i.content}}</view>
-				</view>
-			</view>
-		</view>
-		<view class="top_view">
-			<view class="qz"></view>
-			<view class="title_view2">日常预治小知识</view>
-		</view>
-		<view class="bottom_view">
-			<view class="content_view" v-for="(item, index) in content" :key="index">
-				<view class="circle"></view>
-				<view class="title_view3">{{item}}</view>
-			</view>
-		</view>
-	</view>
+  <view class="content">
+    <view v-if="!isLogin" class="loginContent">
+      <view class="loginContent1">
+        <view class="header_img"
+          ><image src="~@/static/CC_55.png"></image
+        ></view>
+        <view class="erse">欢迎使用</view>
+        <view class="erse margin_left_10">
+          <navigator url="pages/mycenter/regster" open-type="navigate">
+			 <view class="uni-link-item"> 登录/注册</view>
+          </navigator>
+        </view>
+        <view class="erse erse1"
+          ><image src="~@/static/CC_52.png"></image
+        ></view>
+      </view>
+      <view class="loginContent2">
+        <view class="loginContent2_div">
+          <view class="conetns_left"
+            ><view class="conetns_left_img"
+              ><image src="~@/static/CC_43.png"></image
+            ></view>
+            <view class="conetns_left_font">关于系统</view></view
+          >
+          <view class="conetns_right"
+            ><image src="~@/static/CC_62.png"></image
+          ></view>
+        </view>
+      </view>
+    </view>
+    <!-- 登录 -->
+    <view v-else class="loginContent">
+      <view class="loginContent1">
+        <view class="header_img"
+          ><image src="~@/static/CC_55.png"></image
+        ></view>
+        <view class="erse">
+          <view class="">xxx</view>
+          <view class="">男 36岁</view>
+        </view>
+        <view class="erse margin_left_10 Certification">
+          <image src="~@/static/CC_38.png"></image>
+        </view>
+        <view class="erse erse1"
+          ><image src="~@/static/CC_52.png"></image
+        ></view>
+      </view>
+      <view class="loginContent2">
+        <view class="loginContent2_div">
+          <view class="conetns_left"
+            ><view class="conetns_left_img"
+              ><image src="~@/static/CC_49.png"></image
+            ></view>
+            <view class="conetns_left_font">干眼管理</view></view
+          >
+          <view class="conetns_right"
+            ><image src="~@/static/CC_62.png"></image
+          ></view>
+        </view>
+        <view class="loginContent2_div-line"></view>
+        <view class="loginContent2_div">
+          <view class="conetns_left"
+            ><view class="conetns_left_img"
+              ><image src="~@/static/CC_46.png"></image
+            ></view>
+            <view class="conetns_left_font">通知消息</view></view
+          >
+          <view class="conetns_right"
+            ><image src="~@/static/CC_62.png"></image
+          ></view>
+        </view>
+        <view class="loginContent2_div-line"></view>
+        <view class="loginContent2_div">
+          <view class="conetns_left"
+            ><view class="conetns_left_img"
+              ><image src="~@/static/CC_43.png"></image
+            ></view>
+            <view class="conetns_left_font">关于系统</view></view
+          >
+          <view class="conetns_right"
+            ><image src="~@/static/CC_62.png"></image
+          ></view>
+        </view>
+      </view>
+
+      <button type="default" class="login_Outs">退出登录</button>
+    </view>
+  </view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				message: [
-					{title: '复诊提醒', content: '2020年12月20日需进行复诊复诊进行复诊复诊'},
-					{title: '日常护理', content: '要御寒保暖，保护身体，免受免受'},
-					{title: '日常护理', content: '洗脸时注意眼睑及睫毛之清洁，用，用'},
-					{title: '复诊提醒', content: '2020年12月20日需进行复诊复诊'},
-					{title: '复诊提醒', content: '2020年12月20日需进行复诊复诊'},
-					{title: '复诊提醒', content: '2020年12月20日需进行复诊复诊'},
-					{title: '复诊提醒', content: '2020年12月20日需进行复诊复诊'},
-					{title: '日常护理', content: '洗脸时注意眼睑及睫毛之清洁，用，用'}
-				],
-				content: [
-						'老花镜不能随便在路边买，和近视眼镜一样，要验光验配？',
-						'做了近视手术后，就不能再做其他的眼睛手术了？',
-						'角膜太薄，做不了近视激光手术，只能戴眼镜？',
-						'得糖尿病久了，可能会失明？',
-						'角膜移植和其他器官一样需要配对吗？',
-						'散光只能戴眼镜，不能做手术矫正？',
-					'老花镜不能随便在路边买，和近视眼镜一样，要验光验配？',
-					'做了近视手术后，就不能再做其他的眼睛手术了？',
-					'角膜太薄，做不了近视激光手术，只能戴眼镜？',
-					'得糖尿病久了，可能会失明？',
-					'角膜移植和其他器官一样需要配对吗？',
-					'散光只能戴眼镜，不能做手术矫正？'
-				],
-				width: 0,
-				indicatorDots: true,
-				autoplay: true,
-				interval: 2000,
-				duration: 500
-			}
-		},
-		onLoad() {
-			this.width = document.body.clientWidth
-		},
-		methods: {
-			computedWidth (px) {
-				return `calc(${this.width}px - ${px})`
-			}
-		}
-	}
+export default {
+  data() {
+    return {
+      message: [],
+      width: 0,
+      indicatorDots: true,
+      autoplay: true,
+      interval: 2000,
+      duration: 500,
+      isLogin: false,
+    };
+  },
+  onLoad() {
+    this.width = document.body.clientWidth;
+  },
+  methods: {
+    computedWidth(px) {
+      return `calc(${this.width}px - ${px})`;
+    },
+  },
+};
 </script>
 
 <style lang="less" scoped>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-		.swiper{
-			height: 90px;
-			/deep/.uni-swiper-dot-active{
-				border: 2px solid white;
-			}
-		}
-		.uni-flex{
-			display: flex;
-			width: 92%;
-			.uni-flex-item{
-				width: 100%;
-				height: 120px;
-				overflow: auto;
-				.image_view{
-				}
-				.list_view{
-					font-size: 15px;
-					display: flex;
-					margin: 5px 0 5px 10px;
-					position: relative;
-					.status_view{
-						width: 10px;
-						height: 10px;
-						border-radius: 100%;
-						background: red;
-						position: absolute;
-						left: -2.5px;
-						top: -2.5px;
-					}
-					.title{
-						width: 80px;
-						background: #00adaf;
-						color: white;
-						text-align: center;
-						border-radius: 5px;
-						padding: 2px 0;
-					}
-					.content{
-						margin-left: 5px;
-						position: relative;
-						width: 70%;
-						display: inline !important;
-						align-items: normal !important;
-						white-space:nowrap;
-						overflow:hidden;
-						text-overflow:ellipsis;
-					}
-				}
-			}
-		}
-		.top_view{
-			margin-top: 10px;
-			border-bottom: 1px solid #00adaf;
-			padding-bottom: 15px;
-			width: 90%;
-			.qz{
-				background: #00adaf;
-				width: 4px;
-				height:20px;
-				display: inline-block;
-				position: relative;
-				top: 5px;
-				border-radius: 5px;
-			}
-			.title_view2{
-				display: inline-block;
-				color: #00adaf;
-				font-weight: bold;
-				font-size: 16px;
-				margin-left: 15px;
-			}
-		}
-		.bottom_view{
-			padding: 5px 20px;
-			width: 90%;
-			overflow: auto;
-			margin-bottom: 10px;
-			.content_view{
-				padding: 15px 0;
-				display: flex;
-				width: 100%;
-				position: relative;
-				border-bottom: 1px solid rgba(0, 0, 0, .1);
-				.circle{
-					background: #00adaf;
-					width: 10px;
-					height:10px;
-					border-radius: 2px;
-					display: inline-block;
-					position: relative;
-				}
-				.title_view3{
-					width: 90%;
-					font-size: 15px;
-					position: relative;
-					white-space:nowrap;
-					overflow:hidden;
-					text-overflow:ellipsis;
-					margin-left: 10px;
-					margin-top: -5px;
-				}
-			}
-		}
-	}
+.content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  .loginContent {
+    width: 100%;
+    height: 120px;
+    padding: 10px;
+    .login_Outs {
+      width: 70%;
+      margin: 30px auto;
+      background: #00adaf;
+      color: #fff;
+      border-radius: 32px;
+      height: 35px;
+      line-height: 35px;
+    }
+  }
+  .loginContent1 {
+    background: linear-gradient(90deg, #00adaf, #00d7da);
+    color: #fff;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin: 0 auto;
+    border-radius: 12px;
+    box-shadow: 4px 0px 0px #00d7da;
+    width: 90%;
+    .header_img {
+      width: 80px;
+      height: 80px;
+      margin: 8px;
+      uni-image {
+        display: contents;
+      }
+    }
+    .margin_left_10 {
+      margin-left: 15%;
+    }
+    .erse1 {
+      margin-left: 1%;
+      uni-image {
+        display: contents;
+      }
+      width: 10px;
+      height: 20px;
+    }
+    .Certification {
+      width: 95px;
+      height: 32px;
+      uni-image {
+        display: contents;
+      }
+    }
+  }
+  .loginContent2 {
+    margin: 10px auto;
+    width: 93%;
+    border: 1px solid #e5e5e5;
+    border-radius: 8px;
+    padding: 0 8px;
+
+    .loginContent2_div {
+      margin: 10px 4px;
+      align-items: center;
+      display: flex;
+    }
+    .conetns_left {
+      flex-grow: 1;
+      display: flex;
+      .conetns_left_img {
+        width: 30px;
+        height: 30px;
+        uni-image {
+          display: contents;
+        }
+      }
+      .conetns_left_font {
+        margin-left: 3%;
+      }
+    }
+    .loginContent2_div-line {
+      border: 1px solid #e5e5e5;
+    }
+    .conetns_right {
+      width: 10px;
+      height: 15px;
+      uni-image {
+        display: contents;
+      }
+    }
+  }
+}
 </style>
---
+++ src/pages/mycenter/regster.vue
@@ -0,0 +1,89 @@
+<template>
+	<view class="content">
+		<view class="input-group">
+			<view class="input-row border">
+				<text class="title">姓  名：</text>
+				<input type="text" focus clearable v-model="UserName" placeholder="请输入姓名"></m-input>
+			</view>
+			<view class="input_line"></view>
+			<view class="input-row border">
+				<text class="title">性  别：</text>
+				<input type="text" focus clearable v-model="UserName" placeholder="请输入性别"></m-input>
+			</view>
+			<view class="input_line"></view>
+			<view class="input-row border">
+				<text class="title">出生日期：</text>
+				<input type="text" focus clearable v-model="UserName" placeholder="请输入出生日期："></m-input>
+			</view>
+			<view class="input_line"></view>
+			<view class="input-row border">
+				<text class="title">身份证号：</text>
+				<input type="text" focus clearable v-model="UserName" placeholder="请输入身份证号"></m-input>
+			</view>
+			<view class="input_line"></view>
+			<view class="input-row border">
+				<text class="title">手 机 号：</text>
+				<input type="text" focus clearable v-model="UserName" placeholder="请输入手机号"></m-input>
+			</view>
+			
+		</view>
+		<view class="btn-row">
+			<button class="cu-btn bg-green block lg" @tap="register">确  定</button>
+			<button class="cu-btn bg-green block lg canuer" @tap="register">取  消</button>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				UserName: '',
+				password: '',
+				email: ''
+			}
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="less" scoped>
+	.content {
+		.input-group {
+			width: 93%;
+			margin: 5px auto;
+			border: 1px solid #e5e5e5;
+			border-radius: 4px;
+			padding: 5px;
+			.input-row {
+				display: flex;
+				padding: 6px 8px;
+				.title {
+					color: #0b8e90;
+				}
+			}
+			.input_line {
+				border: 1px solid #e5e5e5;
+			}
+		}
+		.btn-row {
+			display: flex;
+			.cu-btn {
+				width: 40%;
+				margin: 30px auto;
+				background: #00adaf;
+				color: #fff;
+				border-radius: 32px;
+				height: 35px;
+				line-height: 35px;
+				box-shadow: 3px 0 3px #e5e5e5;
+			}
+			.canuer {
+				background: #aaaaaa;
+			}
+		}
+		
+	}
+</style>
